2089048575e3b7609878bf9389648f730d80c120
Brian Salcedo <brian@salcedo.tech>
2019-04-28 20:04:05 -0400

    furether updates

--- plugin.py
+++ plugin.py
@@ -4,28 +4,26 @@ import requests
 
 
 if __name__ == '__main__':
-    url = (os.getenv('PLUGIN_URL') or '') + '/api'
+    url = os.environ['PLUGIN_URL'] + '/api'
     username = os.environ['PLUGIN_USERNAME']
     password = os.environ['PLUGIN_PASSWORD']
-    stack = os.getenv('PLUGIN_STACK') or ''
-    endpoint = os.getenv('PLUGIN_ENDPOINT') or 'primary'
+    stack = os.environ['PLUGIN_STACK']
 
     print('updating stack: ' + stack)
 
-    jwt = requests.post(
-        url + '/auth',
-        json={
-            'Username': username,
-            'Password': password
-        }
-    ).json()['jwt']
+    credentials = {
+        'Username': username,
+        'Password': password
+    }
+
+    jwt = requests.post(url + '/auth', json=credentials).json()['jwt']
 
     headers = {
         'Authorization': 'Bearer ' + jwt
     }
 
     for e in requests.get(url + '/endpoints', headers=headers).json():
-        if e['Name'] == endpoint:
+        if e['Name'] == 'primary':
             endpointId = str(e['Id'])
 
     for s in requests.get(url + '/stacks', headers=headers).json():
